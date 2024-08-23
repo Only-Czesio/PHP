10c5534cbf5c94c432df5559469e94a06d1ddc4b
OnlyCzesio <marek@kamyk.me>
2024-08-23 20:30:14 +0200

    Hello World

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
-<?php
-    $zmienna = "Eric";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHPTutorial</title>
+</head>
+<body>
+ <?php
+    $PHP_stands_for = "PHP: Hypertext Preprocessor";
 
-    echo 'Witam' . $zmienna . 'Dobry dzień';
-?>
+    echo '<h1>What does the PHP stands for? </h1>' . $PHP_stands_for. "<br /> \n <p>It's using recursive approach in the name. How cool is that!";
+
+    $PHP_rules = "Code goes between start and end processing instructions <?php {Code go here} ?>";
+
+    echo 'Some rules: ' . $PHP_rules;
+
+    $PHP_as_server_side_lang = "I run on server before you can see my magic!";
+
+    echo $PHP_as_server_side_lang;
+
+
+?>   
+</body>
+</html>
